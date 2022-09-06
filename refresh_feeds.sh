e41a799a6a6eb7130ba2da15da2dffdf95704d4e
JackyYe <44722606+JackJackYe@users.noreply.github.com>
2022-09-06 22:51:13 +0800

    Update refresh_feeds.sh

--- refresh_feeds.sh
+++ refresh_feeds.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm -rf feeds/*.index feeds/*.targetindex feeds/*.tmp packages/feeds
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+git clone https://github.com/Zxilly/UA2F package/UA2F
 ./scripts/feeds update -i
 ./scripts/feeds install -a
 git add feeds package/feeds
